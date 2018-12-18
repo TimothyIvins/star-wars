070a00bbbb2e371c066d9d5920a648503e2459e5
Timothy Ivins <43072347+TimothyIvins@users.noreply.github.com>
2018-12-18 14:47:02 -0500

    Added Redux for search field state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { setSearchField } from '../actions';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 // import Scroll from '../components/Scroll';
 // import './App.css';
 
+// parameter state comes from index.js provider store state(rootReducers)
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField
+  }
+};
+
+// dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
+// the function returns an object then uses connect to change the data from redecers.
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  }
+};
 
 class App extends React.Component {
 	constructor() {
 		super();
 		// this.url = url;
 		this.state = {
-			swObjects: [],
-			searchField: ''
+			swObjects: []
 		};
 	};
 	
@@ -28,25 +43,22 @@ class App extends React.Component {
     	}
   	}
 
-	onSearchChange = (event) => {
-		this.setState({ searchField: event.target.value });
-	};
-	
 	render() {
-		const { swObjects, searchField } = this.state;
+		const { swObjects } = this.state;
+		const { searchField, onSearchChange, type } = this.props;
 		const filteredSWObjects = swObjects.filter(swobject => {
-			return !(this.props.type === 'films') ?
+			return !(type === 'films') ?
 				swobject.name.toLowerCase().includes(searchField.toLowerCase()) :
 				swobject.title.toLowerCase().includes(searchField.toLowerCase());
 		});
   		return !swObjects.length ?
   			<h3 className='yellow'>Loading..</h3> :
 			(<div className='tc'>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList swobjects={filteredSWObjects} type={this.props.type}/>
+				<SearchBox searchChange={onSearchChange}/>
+				<CardList swobjects={filteredSWObjects} type={type}/>
 			</div>
 			);
 	}
 };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { searchItems } from './reducers'
 import Main from "./components/Main";
 import "./index.css";
 import 'tachyons';
+
+const store = createStore(searchItems);
  
 ReactDOM.render(
-  <Main />, 
-  document.getElementById("root")
+	<Provider store={store}>
+    	<Main />
+  	</Provider>,
+	document.getElementById("root")
 );
